f9238182640dee1c0e05dd302b7306b05f6b71a9
valentinchavez <valeonchavez@gmail.com>
2022-12-29 15:04:04 -0300

    formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import ShopProvider from './contexts/ShopContext';
 import MainNavigator from './navigation/MainNavigator';
+import Formulario from './Formulario';
+
 
 function App() {
 
   return (
     <ShopProvider>
+      <Formulario/>
       <MainNavigator/>
     </ShopProvider>
   );
